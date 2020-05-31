b15bcb220f1fcd4d4b5f55cdc436e2f784de1e8e
z2tb8e@inf.elte.hu <z2tb8e@inf.elte.hu>
2020-05-31 14:08:21 +0200

    Images for display are now converted to byte arrays instead of bitmapimagesources, changed converter accordingly

---
+++ GalaxyTrucker/Views/Utils/ImageToByteArrayConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace GalaxyTrucker.Views.Utils
+
+{
+    [ValueConversion(typeof(Image), typeof(byte[]))]
+    public class ImageToByteArrayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is null || !(value is Image myImage))
+            {
+                return null;
+            }
+            using var ms = new MemoryStream();
+            myImage.Save(ms, myImage.RawFormat);
+            return ms.ToArray();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
